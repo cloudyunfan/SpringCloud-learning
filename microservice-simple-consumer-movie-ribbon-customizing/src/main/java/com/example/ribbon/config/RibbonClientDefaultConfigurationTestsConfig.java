88adabedff6bac8a875af20decc8ad21d16ca36d
cloudyunfan <yunfan2clouyf@gmail.com>
2019-03-28 22:59:14 +0800

    提交所有

---
+++ microservice-simple-consumer-movie-ribbon-customizing/src/main/java/com/example/ribbon/config/RibbonClientDefaultConfigurationTestsConfig.java
@@ -0,0 +1,45 @@
+package com.example.ribbon.config;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.ConfigurationBasedServerList;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
+import com.netflix.loadbalancer.ServerListSubsetFilter;
+
+@RibbonClients(defaultConfiguration = DefaultRibbonConfig.class)
+public class RibbonClientDefaultConfigurationTestsConfig {
+	public static class BazServiceList extends ConfigurationBasedServerList {
+		public BazServiceList(IClientConfig config) {
+			super.initWithNiwsConfig(config);
+		}
+	}
+}
+
+@Configuration
+class DefaultRibbonConfig {
+	@Bean
+	public IRule ribbonRule() {
+		return new BestAvailableRule();
+	}
+	@Bean
+	public IPing ribbonPing() {
+		return new PingUrl();
+	}
+	@Bean
+	public ServerList<Server> ribbonServerList(IClientConfig config) {
+		return new RibbonClientDefaultConfigurationTestsConfig.BazServiceList(config);
+	}
+	@Bean
+	public ServerListSubsetFilter<?> serverListSubsetFilter() {
+		ServerListSubsetFilter<?> filter =new ServerListSubsetFilter<>();
+		return filter;
+	}
+}
